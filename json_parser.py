8aad715985a8648ada6c1be817fc737d47c0acd2
John Calabrese <jcalabrese@pivotal.io>
2014-09-11 22:55:22 -0600

    adding tests and wercker config
    we now have some basic unit tests
    and wercker steps to go with it

--- json_parser.py
+++ json_parser.py
@@ -12,24 +12,30 @@ def readPipeAsString():
 def getJsonObject():
   return json.loads(readPipeAsString())
 
-def findAppNameMatch(name):
-  return re.match( sys.argv[1]+'-([0-9a-f]{5,40})', name, re.M|re.I)
+def findAppNameMatch(appNameArg, name):
+  return re.match( appNameArg+'-([0-9a-f]{5,40})', name, re.M|re.I)
 
 def appIsRunning(state):
   return state != "STOPPED"
 
+def iterateJson(appNameArg, jsonObj):
+  rval = "notfound"
+
+  if "resources" in jsonObj:
+    for i in jsonObj["resources"]:
+      e = i["entity"]
+      name = e["name"]
+
+      if findAppNameMatch(appNameArg, name) and appIsRunning(e["state"]):
+        rval = str(name)+"|"+str(e["instances"])+"|"+str(e["memory"])+"|"+str(e["disk_quota"])
+        break
+
+  return rval
+
 def getApp():
+    appNameArg = sys.argv[1]
     j = getJsonObject()
-    rval = "notfound"
+    return iterateJson(appNameArg, j)
 
-    for i in j["resources"]:
-        e = i["entity"]
-        name = e["name"]
-
-        if findAppNameMatch(name) and appIsRunning(e["state"]):
-            rval = str(name)+"|"+str(e["instances"])+"|"+str(e["memory"])+"|"+str(e["disk_quota"])
-            break
-
-    return rval
-
-print(getApp())
+if __name__ == '__main__':
+  print(getApp())
---
+++ json_parser_unittest.py
@@ -0,0 +1,50 @@
+import unittest
+import json_parser
+import json
+
+class PrimesTestCase(unittest.TestCase):
+    """Tests for `primes.py`."""
+
+    def test_appIsRunning(self):
+        """appIsRunning should return false if stopped status and true if started status"""
+        self.assertFalse(json_parser.appIsRunning("STOPPED"))
+        self.assertTrue(json_parser.appIsRunning("STARTED"))
+
+    def test_findAppNameMatch(self):
+        """findAppNameMatch should return false if no match is found and true if a match is found"""
+        self.assertFalse(json_parser.findAppNameMatch("sample-zdd", "sample-zdd-123"))
+        self.assertTrue(json_parser.findAppNameMatch("sample-zdd", "sample-zdd-ef306b2479a7ecd4337875b4d954a4c8fc18e237"))
+
+    def test_iterateJson(self):
+        """iterateJson should return notfound if no matches in jsonobj
+        or return a pipe delimited string of values if it finds a match in the jsonobj"""
+        validString = """{
+          "resources": [{
+            "entity": {
+              "name": "sample-zdd-ef306b2479a7ecd4337875b4d954a4c8fc18e237",
+              "memory": 1028,
+              "instances": 1,
+              "disk_quota": 1024,
+              "state": "STARTED"
+            }
+          }]
+        }"""
+        invalidState = """{
+          "resources": [{
+            "entity": {
+              "name": "sample-zdd-ef306b2479a7ecd4337875b4d954a4c8fc18e237",
+              "memory": 1028,
+              "instances": 1,
+              "disk_quota": 1024,
+              "state": "STOPPED"
+            }
+          }]
+        }"""
+
+        self.assertEqual(json_parser.iterateJson("sample-zdd", json.loads("{}")), "notfound", "invalid json should return notfound value")
+        self.assertEqual(json_parser.iterateJson("nomatch-value",  json.loads(validString)), "notfound", "no match should return notfound value")
+        self.assertEqual(json_parser.iterateJson("sample-zdd",  json.loads(invalidState)), "notfound", "no match should return notfound value")
+        self.assertEqual(json_parser.iterateJson("sample-zdd",  json.loads(validString)), "sample-zdd-ef306b2479a7ecd4337875b4d954a4c8fc18e237|1|1028|1024","valid json should return the pipe delimeted values we need")
+
+if __name__ == '__main__':
+    unittest.main()
