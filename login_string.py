8529aa5c83539cad59b9b661a75d7b21a3e90759
John Calabrese <jcalabrese@pivotal.io>
2014-10-13 09:11:18 -0400

    [#80305450] login string generation
    the string is being built... badly in
    need of tests for this now

--- login_string.py
+++ login_string.py
@@ -4,16 +4,17 @@ from pipeline_step_base import PipelineStepInterface
 class LoginStringFactory(PipelineStepInterface):
   def __init__(self):
     self.cfg = Config()
+    self.env = {}
 
   def _get_helper(self, var_name):
-        return self.cfg.get(self.cfg.make_name(var_name))
+    return self.env.get(self.cfg.make_name(var_name))
 
   def generate_login_string(self, **kwargs):
-    env = kwargs.get(self.cfg.ENV_VARIABLES)
-    org = env.get(self.cfg.ORG)
-    space = env.get(self.cfg.SPACE)
-    usr_pass = env.get(self.cfg.USER_PASS)
-    usr_name = env.get(self.cfg.USER_NAME)
+    self.env = kwargs.get(self.cfg.ENV_VARIABLES)
+    org = self._get_helper(self.cfg.ORG)
+    space = self._get_helper(self.cfg.SPACE)
+    usr_pass = self._get_helper(self.cfg.USER_PASS)
+    usr_name = self._get_helper(self.cfg.USER_NAME)
     cmd = kwargs.get(self.cfg.CF_CMD)
     return "{0} login -u {1} -p {2} -o {3} -s {4}".format(cmd, usr_name, usr_pass, org, space)
 
