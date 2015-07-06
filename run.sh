b17de6e00ccf00c0db05af384f29d90d6763a162
John Calabrese <jcalabrese@pivotal.io>
2015-07-06 12:16:44 -0400

    fixing var names and version
    upping the version #
    
    fixing the env var names, to match the zdd
    step

--- run.sh
+++ run.sh
@@ -1,104 +1,104 @@
 #!/bin/sh
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_API_URL" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_API_URL" ]
 then
-    fail 'missing or empty option cloudfoundry_api_url, please check wercker.yml'
+    fail 'missing or empty option api_url, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_APP_NAME" ]
 then
-    fail 'missing or empty option cloudfoundry_app_name, please check wercker.yml'
+    fail 'missing or empty option app_name, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_NAME" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_USER_NAME" ]
 then
-    fail 'missing or empty option cloudfoundry_user_name, please check wercker.yml'
+    fail 'missing or empty option user_name, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_USER_PASS" ]
 then
-    fail 'missing or empty option cloudfoundry_user_pass, please check wercker.yml'
+    fail 'missing or empty option user_pass, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_ORG" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_ORG" ]
 then
-    fail 'missing or empty option cloudfoundry_org, please check wercker.yml'
+    fail 'missing or empty option org, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_SPACE" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_SPACE" ]
 then
-    fail 'missing or empty option cloudfoundry_space, please check wercker.yml'
+    fail 'missing or empty option space, please check wercker.yml'
 fi
 
-if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USE_MANIFEST" ]
+if [ ! -n "$WERCKER_CF_PUSH_ZDD_USE_MANIFEST" ]
 then
-    fail 'missing or empty option cloudfoundry_use_manifest, please check wercker.yml'
+    fail 'missing or empty option use_manifest, please check wercker.yml'
 fi
 
 sudo apt-get install -y golang
 wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v6.3.2/cf-linux-amd64.tgz
 tar -zxvf cf-linux-amd64.tgz
 CF=./cf
-${CF} api ${WERCKER_CF_PUSH_CLOUDFOUNDRY_API_URL}
+${CF} api ${WERCKER_CF_PUSH_ZDD_API_URL}
 
-${CF} login -u ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_NAME} -p ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS} -o ${WERCKER_CF_PUSH_CLOUDFOUNDRY_ORG} -s ${WERCKER_CF_PUSH_CLOUDFOUNDRY_SPACE}
+${CF} login -u ${WERCKER_CF_PUSH_ZDD_USER_NAME} -p ${WERCKER_CF_PUSH_ZDD_USER_PASS} -o ${WERCKER_CF_PUSH_ZDD_ORG} -s ${WERCKER_CF_PUSH_ZDD_SPACE}
 
 go get github.com/xchapter7x/autopilot
 ${CF} install-plugin $GOPATH/bin/autopilot
 
 PUSH_CMD=""
-PUSH_CMD="${CF} push-zdd ${WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME}"
+PUSH_CMD="${CF} push-zdd ${WERCKER_CF_PUSH_ZDD_APP_NAME}"
 
-if [[ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USE_MANIFEST} == false ]]; then
+if [[ ${WERCKER_CF_PUSH_ZDD_USE_MANIFEST} == false ]]; then
   PUSH_CMD="${PUSH_CMD} --no-manifest"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_BUILDPACK} ]; then
-  PUSH_CMD="${PUSH_CMD} -b ${WERCKER_CF_PUSH_CLOUDFOUNDRY_BUILDPACK}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_BUILDPACK} ]; then
+  PUSH_CMD="${PUSH_CMD} -b ${WERCKER_CF_PUSH_ZDD_BUILDPACK}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_COMMAND} ]; then
-  PUSH_CMD="${PUSH_CMD} -c ${WERCKER_CF_PUSH_CLOUDFOUNDRY_COMMAND}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_COMMAND} ]; then
+  PUSH_CMD="${PUSH_CMD} -c ${WERCKER_CF_PUSH_ZDD_COMMAND}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_DOMAIN} ]; then
-  PUSH_CMD="${PUSH_CMD} -d ${WERCKER_CF_PUSH_CLOUDFOUNDRY_DOMAIN}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_DOMAIN} ]; then
+  PUSH_CMD="${PUSH_CMD} -d ${WERCKER_CF_PUSH_ZDD_DOMAIN}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NUM_INSTANCES} ]; then
-  PUSH_CMD="${PUSH_CMD} -i ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NUM_INSTANCES}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_NUM_INSTANCES} ]; then
+  PUSH_CMD="${PUSH_CMD} -i ${WERCKER_CF_PUSH_ZDD_NUM_INSTANCES}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_MEMORY} ]; then
-  PUSH_CMD="${PUSH_CMD} -m ${WERCKER_CF_PUSH_CLOUDFOUNDRY_MEMORY}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_MEMORY} ]; then
+  PUSH_CMD="${PUSH_CMD} -m ${WERCKER_CF_PUSH_ZDD_MEMORY}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST} ]; then
-  WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST=`echo ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST} | tr "\/" "-" | tr "\." "-"`
-  PUSH_CMD="${PUSH_CMD} -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_HOST} ]; then
+  WERCKER_CF_PUSH_ZDD_HOST=`echo ${WERCKER_CF_PUSH_ZDD_HOST} | tr "\/" "-" | tr "\." "-"`
+  PUSH_CMD="${PUSH_CMD} -n ${WERCKER_CF_PUSH_ZDD_HOST}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_PATH} ]; then
-  PUSH_CMD="${PUSH_CMD} -p ${WERCKER_CF_PUSH_CLOUDFOUNDRY_PATH}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_PATH} ]; then
+  PUSH_CMD="${PUSH_CMD} -p ${WERCKER_CF_PUSH_ZDD_PATH}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_STACK} ]; then
-  PUSH_CMD="${PUSH_CMD} -s ${WERCKER_CF_PUSH_CLOUDFOUNDRY_STACK}"
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_STACK} ]; then
+  PUSH_CMD="${PUSH_CMD} -s ${WERCKER_CF_PUSH_ZDD_STACK}"
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_HOSTNAME} ]; then
-  if [[ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_HOSTNAME} == true ]]; then
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_NO_HOSTNAME} ]; then
+  if [[ ${WERCKER_CF_PUSH_ZDD_NO_HOSTNAME} == true ]]; then
     PUSH_CMD="${PUSH_CMD} --no-hostname"
   fi
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_ROUTE} ]; then
-  if [[ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_ROUTE} == true ]]; then
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_NO_ROUTE} ]; then
+  if [[ ${WERCKER_CF_PUSH_ZDD_NO_ROUTE} == true ]]; then
     PUSH_CMD="${PUSH_CMD} --no-route"
   fi
 fi
 
-if [ ! -z ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_START} ]; then
-  if [[ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_START} == true ]]; then
+if [ ! -z ${WERCKER_CF_PUSH_ZDD_NO_START} ]; then
+  if [[ ${WERCKER_CF_PUSH_ZDD_NO_START} == true ]]; then
     PUSH_CMD="${PUSH_CMD} --no-start"
   fi
 fi
